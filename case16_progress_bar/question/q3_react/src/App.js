bad5641f5b8d12fd5b9e438219e9b99efcc7b5ac
seolhyelin <tosiltosilkk@gmail.com>
2023-03-09 16:05:42 +0900

    Feat:progress bar 기능

--- case16_progress_bar/question/q3_react/src/App.js
+++ case16_progress_bar/question/q3_react/src/App.js
@@ -1,42 +1,48 @@
-
-import { useState  } from 'react';
-import ProgressBar from './ProgressBar';
-import Button from './Button';
+import { useState, useRef } from "react";
+import ProgressBar from "./ProgressBar";
+import Button from "./Button";
 
 function App() {
-  
-  const [ current , setCurrent ] = useState(0);
+  const [current, setCurrent] = useState(0);
   const limit = 4;
   const range = 100 / limit;
   const animationSpeed = 1000;
+  const isLoading = useRef(false);
 
-  const handleNext = async() => {
+  const delay = () => {
+    isLoading.current = true;
+    return new Promise(() =>
+      setTimeout(() => (isLoading.current = false), animationSpeed)
+    );
+  };
+
+  const handleNext = async () => {
     // Do Something Here!
     // 다음 스텝으로 이동
-    
+    if (isLoading.current) return;
+    if (current === limit) return;
+    setCurrent(current + 1);
+    await delay();
+  };
 
-  }
-
-  const handlePrev = async() => {
+  const handlePrev = async () => {
     // Do Something Here!
     // 이전 스텝으로 이동
-
-  }
-
-
+    if (isLoading.current) return;
+    if (current === 0) return;
+    setCurrent(current - 1);
+    await delay();
+  };
 
   return (
     <div>
-
-      <ProgressBar width={ current * range } animationSpeed={animationSpeed}/>
+      <ProgressBar width={current * range} animationSpeed={animationSpeed} />
 
       <br />
       <br />
 
-
-      <Button text={'이전'} onClick={handlePrev}/>
-      <Button text={'다음'} onClick={handleNext}/>
-
+      <Button text={"이전"} onClick={handlePrev} />
+      <Button text={"다음"} onClick={handleNext} />
     </div>
   );
 }
