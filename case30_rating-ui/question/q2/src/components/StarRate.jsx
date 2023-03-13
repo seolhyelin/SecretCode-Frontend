07f8a042b9edaaea9448139ff2a30de84509f8f5
seolhyelin <tosiltosilkk@gmail.com>
2023-03-13 17:27:54 +0900

    Feat: rating 숫자표시

--- case30_rating-ui/question/q2/src/components/StarRate.jsx
+++ case30_rating-ui/question/q2/src/components/StarRate.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import './StarRate.scss'
-import EmptyStar from '../../assets/star-empty.svg'
-import HalfStar from '../../assets/star-half.svg'
-import FullStar from '../../assets/star-full.svg'
-import Reset from '../../assets/reset.svg'
+import React from "react";
+import "./StarRate.scss";
+import EmptyStar from "../../assets/star-empty.svg";
+import HalfStar from "../../assets/star-half.svg";
+import FullStar from "../../assets/star-full.svg";
+import Reset from "../../assets/reset.svg";
 
 // 별의 갯수 = Score
-const MAX_SCORE = 5
+const MAX_SCORE = 5;
 
 export const StarRate = ({ onChange, score }) => {
   // Write your solution here
-
-  return <></>
-}
+  return (
+    <div className="star-container">
+      {[...Array(MAX_SCORE)].map((star, index) => {
+        return (
+          <button key={index} onClick={() => onChange(index + 1)}>
+            <EmptyStar />
+          </button>
+        );
+      })}
+      <Reset />
+    </div>
+  );
+};
